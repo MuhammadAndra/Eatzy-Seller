b087826f62177fa5340486eec90b87116f2d856a
MuhammadAndra <m.andradzaki@gmail.com>
2025-05-03 11:59:53 +0700

    add: bottom nav bar

--- app/src/main/java/com/example/eatzy_seller/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/eatzy_seller/navigation/AppNavigation.kt
@@ -4,18 +4,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.example.eatzy_seller.navigation.navGraph.Home
 import com.example.eatzy_seller.navigation.navGraph.Login
 import com.example.eatzy_seller.navigation.navGraph.Test
 import com.example.eatzy_seller.navigation.navGraph.authGraph
 import com.example.eatzy_seller.navigation.navGraph.homeGraph
+import com.example.eatzy_seller.navigation.navGraph.menuGraph
+import com.example.eatzy_seller.navigation.navGraph.orderGraph
 import com.example.eatzy_seller.navigation.navGraph.testGraph
 
 @Composable
 fun AppNavigation(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Test) {
+    NavHost(navController = navController, startDestination = Home) {
         authGraph(navController)
         homeGraph(navController)
         testGraph(navController)
+        orderGraph(navController)
+        menuGraph(navController)
     }
 }
--- app/src/main/java/com/example/eatzy_seller/navigation/navGraph/HomeNavigation.kt
+++ app/src/main/java/com/example/eatzy_seller/navigation/navGraph/HomeNavigation.kt
@@ -4,6 +4,8 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.example.eatzy_seller.ui.screen.canteen.CanteenScreen
+import com.example.eatzy_seller.ui.screen.home.HomeScreen
+import com.example.eatzy_seller.ui.screen.profile.ProfileScreen
 
 import kotlinx.serialization.Serializable
 
@@ -13,6 +15,12 @@ object Search
 @Serializable
 object Canteen
 
+@Serializable
+object Home
+
+@Serializable
+object Profile
+
 fun NavGraphBuilder.homeGraph(navController: NavController) {
     composable<Search> {
 
@@ -20,4 +28,13 @@ fun NavGraphBuilder.homeGraph(navController: NavController) {
     composable<Canteen> {
         CanteenScreen(onNavigateToLogin = { navController.navigate(Login) })
     }
+
+    composable<Home> {
+        HomeScreen(navController = navController)
+    }
+
+    composable<Profile> {
+        ProfileScreen(navController = navController)
+    }
+
 }
